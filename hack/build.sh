68256d73c1821ef12cb7a7d63584b8d72b21f222
Chun Chen <ramichen@tencent.com>
2016-10-12 01:12:53 +0800

    Build loopback plugin

--- hack/build.sh
+++ hack/build.sh
@@ -6,16 +6,17 @@ bin_prefix="galaxy"
 package=git.code.oa.com/gaiastack/galaxy
 cni_package=github.com/containernetworking/cni
 
-echo "Building ipam plugins"
-# build host-local ipam plugin
-go_build_hostlocal="-o /go/src/$package/bin/${bin_prefix}-ipam-local -v $cni_package/plugins/ipam/host-local"
 mkdir -p `dirname /go/src/$cni_package`
 ln -s /go/src/$package/vendor/$cni_package /go/src/$cni_package
+echo "Building ipam plugins"
 echo "   host-local"
-go build $go_build_hostlocal
-# TODO loopback plugin
-
+# build host-local ipam plugin
+go build -o /go/src/$package/bin/${bin_prefix}-ipam-local -v $cni_package/plugins/ipam/host-local
 echo "Building plugins"
+echo "   loopback"
+go build -o /go/src/$package/bin/${bin_prefix}-loopback -v $cni_package/plugins/main/loopback
+
+# build galaxy cni plugins
 PLUGINS="/go/src/$package/cni/*"
 for d in $PLUGINS; do
 	if [ -d $d ]; then
