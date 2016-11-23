a8a22efcae4dd74c07fb0c324f1870cd00e9d2f9
Chun Chen <ramichen@tencent.com>
2016-11-23 15:06:11 +0800

    Save port info on cmdAdd

--- cni/k8s-cni/k8s_cni.go
+++ cni/k8s-cni/k8s_cni.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"runtime"
 
 	"github.com/containernetworking/cni/pkg/invoke"
@@ -11,6 +14,10 @@ import (
 	"github.com/containernetworking/cni/pkg/version"
 
 	"git.code.oa.com/gaiastack/galaxy/pkg/api/k8s"
+)
+
+var (
+	stateDir = "/var/lib/cni/galaxy"
 )
 
 type NetConf struct {
@@ -37,19 +44,6 @@ func loadConf(bytes []byte) (*NetConf, error) {
 }
 
 func cmdAdd(args *skel.CmdArgs) error {
-	return cmd(args, true)
-}
-
-func cmdDel(args *skel.CmdArgs) error {
-	if args.Netns == "" {
-		// avoid k8s double delete error
-		// see https://github.com/kubernetes/kubernetes/issues/20379#issuecomment-255272531
-		return nil
-	}
-	return cmd(args, false)
-}
-
-func cmd(args *skel.CmdArgs, add bool) error {
 	conf, err := loadConf(args.StdinData)
 	if err != nil {
 		return err
@@ -59,7 +53,7 @@ func cmd(args *skel.CmdArgs, add bool) error {
 		return err
 	}
 	for networkType, delegate := range conf.NetworkType {
-		result, err := delegateCmd(kvMap[k8s.K8S_POD_INFRA_CONTAINER_ID], delegate, add)
+		result, err := delegateCmd(kvMap[k8s.K8S_POD_INFRA_CONTAINER_ID], delegate, true)
 		if err != nil {
 			return fmt.Errorf("failed to delegate setup network %s: %v", networkType, err)
 		}
@@ -67,37 +61,65 @@ func cmd(args *skel.CmdArgs, add bool) error {
 		if err != nil {
 			return err
 		}
-		if add {
-			// we have to fulfill ip field of the current pod
-			if result.IP4 == nil {
-				return fmt.Errorf("CNI plugin reported no IPv4 address")
+		if err := savePort(kvMap[k8s.K8S_POD_INFRA_CONTAINER_ID], kvMap[k8s.K8S_PORTS]); err != nil {
+			return fmt.Errorf("failed to save ports %v", err)
+		}
+		// we have to fulfill ip field of the current pod
+		if result.IP4 == nil {
+			return fmt.Errorf("CNI plugin reported no IPv4 address")
+		}
+		ip4 := result.IP4.IP.IP.To4()
+		if ip4 == nil {
+			return fmt.Errorf("CNI plugin reported an invalid IPv4 address: %+v.", result.IP4)
+		}
+		for _, p := range ports {
+			if p.PodName == kvMap[k8s.K8S_POD_NAME]+"_"+kvMap[k8s.K8S_POD_NAMESPACE] {
+				p.PodIP = ip4.String()
 			}
-			ip4 := result.IP4.IP.IP.To4()
-			if ip4 == nil {
-				return fmt.Errorf("CNI plugin reported an invalid IPv4 address: %+v.", result.IP4)
-			}
-			for _, p := range ports {
-				if p.PodName == kvMap[k8s.K8S_POD_NAME]+"_"+kvMap[k8s.K8S_POD_NAMESPACE] {
-					p.PodIP = ip4.String()
-				}
-			}
+		}
+		if len(ports) != 0 {
 			if err := setupPortMapping("cni0", ports); err != nil {
-				cleanPortMapping("cni0", ports)
 				return fmt.Errorf("failed to setup port mapping %v: %v", ports, err)
 			}
-		} else {
+		}
+		//TODO send http request for l5 setup
+		return result.Print()
+	}
+	return fmt.Errorf("no network configured")
+}
+
+func cmdDel(args *skel.CmdArgs) error {
+	if args.Netns == "" {
+		// avoid k8s double delete error
+		// see https://github.com/kubernetes/kubernetes/issues/20379#issuecomment-255272531
+		return nil
+	}
+	conf, err := loadConf(args.StdinData)
+	if err != nil {
+		return err
+	}
+	kvMap, err := k8s.ParseK8SCNIArgs(args.Args)
+	if err != nil {
+		return err
+	}
+	for networkType, delegate := range conf.NetworkType {
+		_, err := delegateCmd(kvMap[k8s.K8S_POD_INFRA_CONTAINER_ID], delegate, false)
+		if err != nil {
+			return fmt.Errorf("failed to delegate delete network %s: %v", networkType, err)
+		}
+		ports, err := consumePort(kvMap[k8s.K8S_POD_INFRA_CONTAINER_ID])
+		if err != nil {
+			return fmt.Errorf("failed to read ports %v", err)
+		}
+		if len(ports) != 0 {
 			if err := cleanPortMapping("cni0", ports); err != nil {
-				setupPortMapping("cni0", ports)
 				return fmt.Errorf("failed to delete port mapping %v: %v", ports, err)
 			}
 		}
 		//TODO send http request for l5 setup
-		if add {
-			return result.Print()
-		}
 		return nil
 	}
-	return fmt.Errorf("no network configured")
+	return nil
 }
 
 func delegateCmd(cid string, netconf map[string]interface{}, add bool) (*types.Result, error) {
@@ -118,4 +140,30 @@ func delegateCmd(cid string, netconf map[string]interface{}, add bool) (*types.R
 
 func main() {
 	skel.PluginMain(cmdAdd, cmdDel, version.Legacy)
+}
+
+func savePort(containerID string, portStr string) error {
+	if err := os.MkdirAll(stateDir, 0700); err != nil {
+		return err
+	}
+	path := filepath.Join(stateDir, containerID)
+	return ioutil.WriteFile(path, []byte(portStr), 0600)
+}
+
+func consumePort(containerID string) ([]*k8s.Port, error) {
+	path := filepath.Join(stateDir, containerID)
+	defer os.Remove(path)
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	if len(data) == 0 {
+		return nil, nil
+	}
+	var ports []*k8s.Port
+	if err := json.Unmarshal(data, &ports); err != nil {
+		return nil, err
+	}
+	return ports, nil
 }
