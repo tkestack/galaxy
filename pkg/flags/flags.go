590203cc7c19e0fb34dfff4412577366722027d9
Chun Chen <ramichen@tencent.com>
2017-03-29 15:53:04 +0800

    Support IP format of iface flag

--- pkg/flags/flags.go
+++ pkg/flags/flags.go
@@ -13,7 +13,7 @@ import (
 )
 
 var (
-	flagIface *string = flag.String("iface", "eth1,docker,bond1", "interface to use (CIDR or name) for inter-host communication")
+	flagIface *string = flag.String("iface", "eth1,docker,bond1", "interface to use (IP or CIDR or name) for inter-host communication")
 	nodeIP    string
 	once      sync.Once
 )
@@ -24,10 +24,34 @@ func GetNodeIP() string {
 		if nodeIP == "" {
 			glog.Fatal("iface unconfigured")
 		}
+		// try parse as a cidr format
 		_, _, err := net.ParseCIDR(nodeIP)
 		if err == nil {
 			return
 		}
+		// mayebe just an IP without mask
+		ip := net.ParseIP(nodeIP)
+		if ip != nil {
+			links, err := netlink.LinkList()
+			if err != nil {
+				glog.Fatal(err)
+			}
+			for _, link := range links {
+				addrs, err := netlink.AddrList(link, netlink.FAMILY_V4)
+				if err != nil {
+					glog.Error(err)
+					continue
+				}
+				for _, addr := range addrs {
+					if addr.IP.String() == ip.String() {
+						nodeIP = addr.IPNet.String()
+						return
+					}
+				}
+			}
+			glog.Fatalf("couldn't found any device which carries IP %s", nodeIP)
+		}
+		// or a list of device names
 		devices := strings.Split(nodeIP, ",")
 		if len(devices) == 0 {
 			glog.Fatalf("invalid iface configuration %s", nodeIP)
@@ -35,10 +59,12 @@ func GetNodeIP() string {
 		for _, dev := range devices {
 			nic, err := netlink.LinkByName(dev)
 			if err != nil {
+				glog.Error(err)
 				continue
 			}
 			addrs, err := netlink.AddrList(nic, netlink.FAMILY_V4)
 			if err != nil {
+				glog.Error(err)
 				continue
 			}
 			filteredAddr := network.FilterLoopbackAddr(addrs)
