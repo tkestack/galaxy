86e9122a6a084af84ce5dd5438f7784b66751bcb
Chun Chen <ramichen@tencent.com>
2017-10-26 16:34:52 +0800

    uncomment upload rpm

--- hack/build-rpm.sh
+++ hack/build-rpm.sh
@@ -40,5 +40,5 @@ docker cp ${CURDIR}/bin/${NAME}-${VERSION}.tar.gz ${CONTAINER_NAME}:/root/rpmbui
 docker cp ${CURDIR}/hack/v${V}/galaxy.spec ${CONTAINER_NAME}:/root/rpmbuild/SPECS/
 docker start -ai ${CONTAINER_NAME}
 size=$(ls -l ${CURDIR}/bin/x86_64/${RPMNAME} | awk '{print $5}')
-#curl -v 'http://gaia.repo.oa.com/upload_file?filesize='${size}'&filename='${RPMNAME}'&dirtype=1' -T ${RPMFILE}
-#curl -v 'http://gaia.repo.oa.com/update_repo?dirtype=1'
+curl -v 'http://gaia.repo.oa.com/upload_file?filesize='${size}'&filename='${RPMNAME}'&dirtype=1' -T ${RPMFILE}
+curl -v 'http://gaia.repo.oa.com/update_repo?dirtype=1'
