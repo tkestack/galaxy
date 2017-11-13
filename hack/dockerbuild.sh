837d56161a874a19ed22a61a9b1077e609775a7e
Chun Chen <ramichen@tencent.com>
2017-11-13 17:21:45 +0800

    upgrade to golang 1.8.3

--- hack/dockerbuild.sh
+++ hack/dockerbuild.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 package=git.code.oa.com/gaiastack/galaxy
-docker run --rm -v `pwd`:/go/src/$package -w /go/src/$package golang:1.6.2 bash -c /go/src/$package/hack/build.sh
+docker run --rm -v `pwd`:/go/src/$package -w /go/src/$package golang:1.8.3 bash -c /go/src/$package/hack/build.sh
