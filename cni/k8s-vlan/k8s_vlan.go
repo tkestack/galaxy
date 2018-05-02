5512c998ff95551d8016c6cbd30c5166d7de37c7
forrestchen <forrestchen@tencent.com>
2018-05-02 15:31:46 +0800

    send arp for container ip from host
    Signed-off-by: forrestchen <forrestchen@tencent.com>

--- cni/k8s-vlan/k8s_vlan.go
+++ cni/k8s-vlan/k8s_vlan.go
@@ -56,7 +56,11 @@ func cmdAdd(args *skel.CmdArgs) error {
 	}
 	//send Gratuitous ARP to let switch knows IP floats onto this node
 	//ignore errors as we can't print logs and we do this as best as we can
-	utils.SendGratuitousARP(result020, args)
+	if d.PureMode() {
+		utils.SendGratuitousARP(d.Device, result020.IP4.IP.IP.String(), "")
+	} else {
+		utils.SendGratuitousARP(args.IfName, result020.IP4.IP.IP.String(), args.Netns)
+	}
 	result020.DNS = conf.DNS
 	return result020.Print()
 }
