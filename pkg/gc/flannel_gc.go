9cc5a4ee194e1fee4499def7c68bba8ef70e2a16
ramichen <ramichen@tencent.com>
2018-07-07 08:58:27 +0800

    Clean up exited container's leaky ip file and state file

--- pkg/gc/flannel_gc.go
+++ pkg/gc/flannel_gc.go
@@ -16,7 +16,7 @@ import (
 )
 
 var (
-	flagFlannelGCInterval = flag.Duration("flannel_gc_interval", time.Minute, "Interval of executing flannel network gc")
+	flagFlannelGCInterval = flag.Duration("flannel_gc_interval", time.Second*10, "Interval of executing flannel network gc")
 	flagAllocatedIPDir    = flag.String("flannel_allocated_ip_dir", "/var/lib/cni/networks", "IP storage directory of flannel cni plugin")
 	flagGCDirs            = flag.String("gc_dirs", "/var/lib/cni/flannel,/var/lib/cni/galaxy,/var/lib/cni/galaxy/port", "Comma separated configure storage directory of cni plugin, the file names in this directory are container ids")
 )
@@ -57,6 +57,7 @@ func (gc *flannelGC) Run() {
 }
 
 func (gc *flannelGC) cleanupIP() error {
+	glog.Infof("cleanup ip...")
 	fis, err := ioutil.ReadDir(gc.allocatedIPDir)
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -84,17 +85,17 @@ func (gc *flannelGC) cleanupIP() error {
 			}
 			continue
 		}
-		if _, err := gc.dockerCli.InspectContainer(containerId); err != nil {
+		if c, err := gc.dockerCli.InspectContainer(containerId); err != nil {
 			if _, ok := err.(docker.ContainerNotFoundError); ok {
-				if err := os.Remove(ipFile); err != nil && !os.IsNotExist(err) {
-					glog.Warningf("Error deleting leaky ip file %s/%s container: %v", ipFile, containerId, err)
-				} else {
-					if err == nil {
-						glog.Infof("Deleted leaky ip file %s container %s", ipFile, containerId)
-					}
-				}
+				glog.Infof("container %s not found", containerId)
+				removeLeakyIPFile(ipFile, containerId)
 			} else {
 				glog.Warningf("Error inspect container %s: %v", containerId, err)
+			}
+		} else {
+			if c.State != nil && (c.State.Status == "exited" || c.State.Status == "dead") {
+				glog.Infof("container %s(%s) exited %s", c.ID, c.Name, c.State.Status)
+				removeLeakyIPFile(ipFile, containerId)
 			}
 		}
 	}
@@ -102,6 +103,7 @@ func (gc *flannelGC) cleanupIP() error {
 }
 
 func (gc *flannelGC) cleanupGCDirs() error {
+	glog.Infof("cleanup gc_dirs...")
 	for _, dir := range gc.gcDirs {
 		glog.Infof("start cleanup %s", dir)
 		fis, err := ioutil.ReadDir(dir)
@@ -116,20 +118,40 @@ func (gc *flannelGC) cleanupGCDirs() error {
 				continue
 			}
 			file := filepath.Join(dir, fi.Name())
-			if _, err := gc.dockerCli.InspectContainer(fi.Name()); err != nil {
+			if c, err := gc.dockerCli.InspectContainer(fi.Name()); err != nil {
 				if _, ok := err.(docker.ContainerNotFoundError); ok {
-					if err := os.Remove(file); err != nil && !os.IsNotExist(err) {
-						glog.Warningf("Error deleting file %s: %v", file, err)
-					} else {
-						if err == nil {
-							glog.Infof("Deleted file %s", file)
-						}
-					}
+					glog.Infof("container %s not found", fi.Name())
+					removeLeakyStateFile(file)
 				} else {
 					glog.Warningf("Error inspect container %s: %v", fi.Name(), err)
+				}
+			} else {
+				if c.State != nil && (c.State.Status == "exited" || c.State.Status == "dead") {
+					glog.Infof("container %s(%s) exited %s", c.ID, c.Name, c.State.Status)
+					removeLeakyStateFile(file)
 				}
 			}
 		}
 	}
 	return nil
+}
+
+func removeLeakyIPFile(ipFile, containerId string) {
+	if err := os.Remove(ipFile); err != nil && !os.IsNotExist(err) {
+		glog.Warningf("Error deleting leaky ip file %s container %s: %v", ipFile, containerId, err)
+	} else {
+		if err == nil {
+			glog.Infof("Deleted leaky ip file %s container %s", ipFile, containerId)
+		}
+	}
+}
+
+func removeLeakyStateFile(file string) {
+	if err := os.Remove(file); err != nil && !os.IsNotExist(err) {
+		glog.Warningf("Error deleting file %s: %v", file, err)
+	} else {
+		if err == nil {
+			glog.Infof("Deleted file %s", file)
+		}
+	}
 }
