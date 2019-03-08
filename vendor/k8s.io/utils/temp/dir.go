c77090816c06f462e495a6a69bb422d06263080e
Chun Chen <chenchun.feed@gmail.com>
2019-03-08 19:53:42 +0800

    Update vendor to add grpc

--- vendor/k8s.io/utils/temp/dir.go
+++ vendor/k8s.io/utils/temp/dir.go
@@ -34,29 +34,29 @@ type Directory interface {
 	Delete() error
 }
 
-// Dir is wrapping an temporary directory on disk.
-type Dir struct {
+// TempDir is wrapping an temporary directory on disk.
+type TempDir struct {
 	// Name is the name (full path) of the created directory.
 	Name string
 }
 
-var _ Directory = &Dir{}
+var _ Directory = &TempDir{}
 
 // CreateTempDir returns a new Directory wrapping a temporary directory
 // on disk.
-func CreateTempDir(prefix string) (*Dir, error) {
+func CreateTempDir(prefix string) (*TempDir, error) {
 	name, err := ioutil.TempDir("", fmt.Sprintf("%s-", prefix))
 	if err != nil {
 		return nil, err
 	}
 
-	return &Dir{
+	return &TempDir{
 		Name: name,
 	}, nil
 }
 
 // NewFile creates a new file in the specified directory.
-func (d *Dir) NewFile(name string) (io.WriteCloser, error) {
+func (d *TempDir) NewFile(name string) (io.WriteCloser, error) {
 	return os.OpenFile(
 		filepath.Join(d.Name, name),
 		os.O_WRONLY|os.O_CREATE|os.O_TRUNC|os.O_EXCL,
@@ -65,6 +65,6 @@ func (d *Dir) NewFile(name string) (io.WriteCloser, error) {
 }
 
 // Delete the underlying directory, and all of its content.
-func (d *Dir) Delete() error {
+func (d *TempDir) Delete() error {
 	return os.RemoveAll(d.Name)
 }
