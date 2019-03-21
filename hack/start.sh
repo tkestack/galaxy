6bd28449841a33b8b5b659373b3d4b1e3c9eb255
xunqyzhao <xunqyzhao@tencent.com>
2019-03-22 00:51:46 +0800

    docs: update deploy scripts

--- hack/start.sh
+++ hack/start.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-cp /etc/cni/net.d/000-galaxy.conf /host/etc/cni/net.d/
-cp /opt/cni/bin/galaxy-sdn /host/opt/cni/bin/galaxy-sdn
+cp /etc/cni/net.d/00-galaxy.conf /host/etc/cni/net.d/
+cp /opt/cni/bin/* /host/opt/cni/bin/
 /usr/bin/galaxy --logtostderr=false --log-dir=/data/galaxy/logs --v=3 --kubeconfig=/host/etc/kubernetes/kubelet-kubeconfig --route-eni
