f6115ab1d63fd91bd48bf220933cd2558f949dec
louisssgong <louisssgong@tencent.com>
2019-03-21 15:12:39 +0800

    generate float ip crd

---
+++ pkg/ipam/apis/floatip/v1alpha1/types.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"git.code.oa.com/gaiastack/galaxy/pkg/api/galaxy/constant"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +genclient:noStatus
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type FloatIp struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// Spec defines the desired identities of pods in this tapp.
+	Spec   FloatIpSpec `json:"spec"`
+}
+
+type FloatIpSpec struct {
+	//key can be resolved as pool, namespace name, app name, app type and pod name
+	Key string `json:"key"`
+	//attribute used as node ip
+	Attribute string `json:"attribute"`
+	//policy used as
+	Policy constant.ReleasePolicy `json:"policy"`
+	//subnet used as node's subnet
+	Subnet string `json:"subnet"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type FloatIpList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []FloatIp `json:"items"`
+}
+
