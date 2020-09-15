d4d153ce915a4eddcc095d9624258c199bc11aab
iceyao <xiabingyao@tencent.com>
2020-09-15 17:37:10 +0800

    add send gratuitous arp in container namespace
    - use in ipvlan l2 mode

--- cni/k8s-vlan/k8s_vlan.go
+++ cni/k8s-vlan/k8s_vlan.go
@@ -123,7 +123,9 @@ func setupIPVlan(result *t020.Result, vlanId uint16, args *skel.CmdArgs) error {
 
 	if d.IpVlanMode == "l3" || d.IpVlanMode == "l3s" {
 		_ = utils.SendGratuitousARP(d.Device, result.IP4.IP.IP.String(), "", d.GratuitousArpRequest)
+		return nil
 	}
+	_ = utils.SendGratuitousARP(args.IfName, result.IP4.IP.IP.String(), args.Netns, d.GratuitousArpRequest)
 	return nil
 }
 
